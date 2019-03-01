d7938418ac447da17bf64f808ea6cad564f9575e
jyseo <jy836979@gmail.com>
2019-03-01 22:17:58 +0900

    custom action bar 추가

--- app/src/main/java/com/avkd/humible/MainActivity.java
+++ app/src/main/java/com/avkd/humible/MainActivity.java
@@ -1,7 +1,10 @@
 package com.avkd.humible;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
+        View customView =getSupportActionBar().getCustomView();
+
+        Toolbar parent =(Toolbar) customView.getParent();
+        parent.setContentInsetsAbsolute(0,0);
+
+
+//        ImageButton imageButton= (ImageButton)view.findViewById(R.id.action_bar_back);
+//
+//        imageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+
+
     }
 }
